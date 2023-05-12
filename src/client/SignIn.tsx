80cf00f95707cf019b1bba03f8f9fb6a82ce5913
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-05-12 17:17:52 +0800

    Feat: handle login then redirect to /chat

--- src/client/SignIn.tsx
+++ src/client/SignIn.tsx
@@ -1,5 +1,25 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { io } from "socket.io-client";
+
 function SignIn() {
     const roomNames = ["JavaScript", "Python", "PHP", "C#", "Ruby", "Java"];
+    const [username, setUsername] = useState<string>("");
+    const navigate = useNavigate();
+
+    const socket = io();
+
+    const handleUsernameInput = (
+        event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        setUsername(event.target.value);
+    };
+
+    const handleSubmit = () => {
+        socket.emit("login", username);
+        navigate("/chat");
+    };
+
     return (
         <>
             <div className='join-container'>
@@ -7,13 +27,15 @@ function SignIn() {
                     <h1>Ass</h1>
                 </header>
                 <main className='join-main'>
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <div className='form-control'>
                             <label htmlFor='username'>Username</label>
                             <input
                                 type='text'
                                 name='username'
                                 id='username'
+                                value={username}
+                                onChange={handleUsernameInput}
                                 placeholder='Enter username...'
                                 required
                             />
