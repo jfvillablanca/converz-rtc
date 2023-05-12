b6eae34d14dba90a9b93547da3359242122a3180
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-05-12 12:44:14 +0800

    Import logger, http

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import logger from "morgan";
+import http from "http";
 import ViteExpress from "vite-express";
 import { PORT } from "./utils/env";
 import { Server, Socket } from "socket.io";
@@ -9,7 +11,6 @@ const server = http.createServer(app);
 app.get("/hello", (_, res) => {
   res.send("Hello Vite + React + TypeScript!");
 });
-
 
 server.listen(PORT, () =>
     console.log(`Server is listening on port ${PORT}...`)
