d36fa93acd311c1000b4f6e127585f53583a3fd3
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-05-17 07:25:52 +0800

    Feat: namespace event names

---
+++ src/utils/event-namespace.ts
@@ -0,0 +1,4 @@
+export const EVENT_CHAT = "event-chat";
+export const EVENT_LOGIN = "event-login";
+export const EVENT_CHAT_FROM_SERVER = "broadcast-room-on-chat-event";
+export const EVENT_LOGIN_FROM_SERVER = "notify-room-on-login-event";
