3d7386ebee3de964e08b21b2f538883307c84195
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-05-17 06:30:33 +0800

    Feat: set CORS option for server instance

--- src/server/main.ts
+++ src/server/main.ts
@@ -9,7 +9,11 @@ import { ChatMessageType, UserType } from "../utils/types";
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+    cors : {
+        origin: "http://localhost:5173",
+    },
+});
 
 io.on("connection", (socket: Socket) => {
 
