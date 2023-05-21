df7c0026d55450b0ec5ecda6d9711c1b63f282ad
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-05-21 13:19:59 +0800

    Chore: cleanup of unused const

--- src/server/utils/env.ts
+++ src/server/utils/env.ts
@@ -6,7 +6,4 @@ const isProduction = ENVIRONMENT === "production";
 
 export const PORT = (process.env.PORT || 3000) as number;
 
-// NOTE: Unused
 export const URL = isProduction ? undefined : "http://localhost:5173";
-
-export const URL_SERVER = isProduction ? undefined : `http://localhost:${PORT}`;
