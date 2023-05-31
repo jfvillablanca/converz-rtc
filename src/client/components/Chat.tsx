5026deaf7de12f70ff2b155da7e54679bf22ccd3
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-05-31 13:15:41 +0800

    Chore: move ChatBubble to own component

--- src/client/components/Chat.tsx
+++ src/client/components/Chat.tsx
@@ -14,6 +14,7 @@ import {
 } from "../../utils/types";
 import { useAppContext } from "./../App";
 import { socket } from "./../socket";
+import ChatBubble, { BotNotification } from "./ChatBubble";
 
 function Chat() {
     const navigate = useNavigate();
@@ -261,39 +262,6 @@ function Chat() {
             </div>
         </>
     );
-}
-
-function ChatBubble({
-    index,
-    msg,
-}: {
-    index: number;
-    msg: FormattedMessageType;
-}) {
-    const [userAndRoom] = useAppContext();
-    const { username } = userAndRoom;
-    const chatSide = msg.user === username ? "chat-end" : "chat-start";
-    return (
-        <div key={index} className={`chat ${chatSide} mb-2`}>
-            {/* user avatar placeholder */}
-            <div className='chat-image avatar w-10 h-10 mr-2'>
-                <div className='mask mask-hexagon'>
-                    <img src='https://api.dicebear.com/6.x/identicon/svg' />
-                </div>
-            </div>
-            <div className='chat-header mb-2 font-bold'>
-                {msg.user}
-                <time className='opacity-70 ml-3 text-xs'>{msg.time}</time>
-            </div>
-            <p className='chat-bubble chat-bubble-info whitespace-pre-wrap'>
-                {msg.messageBody}
-            </p>
-        </div>
-    );
-}
-
-function BotNotification({ msg }: { msg: FormattedMessageType }) {
-    return <p className='divider'>{msg.messageBody}</p>;
 }
 
 export default Chat;
