7e87554439e098ed9273774dffdce88015455676
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-05-31 15:17:28 +0800

    Feat: add randomized avatar by dicebear api

--- src/client/components/ChatBubble.tsx
+++ src/client/components/ChatBubble.tsx
@@ -10,22 +10,32 @@ function ChatBubble({
 }) {
     const [userAndRoom] = useAppContext();
     const { username } = userAndRoom;
-    const chatSide = msg.user === username ? "chat-end" : "chat-start";
+    const isChatFromCurrentUser = msg.user === username;
+    const chatSide = isChatFromCurrentUser ? "chat-end" : "chat-start";
+
+    const diceBearArtStyle = [
+        "pixel-art",
+        "lorelei",
+        "adventurer",
+        "big-smile",
+        "identicon",
+    ];
+    const generateAvatarURL = `https://api.dicebear.com/6.x/${diceBearArtStyle[1]}/svg?seed=${msg.user}&flip=${isChatFromCurrentUser}`;
+
     return (
         <div key={index} className={`chat ${chatSide} mb-2`}>
-            {/* user avatar placeholder */}
-            <div className='chat-image avatar w-10 h-10 mr-2'>
+            <div className='chat-image row-start-1 avatar w-20 h-20 mr-2'>
                 <div className='mask mask-hexagon'>
-                    <img src='https://api.dicebear.com/6.x/identicon/svg' />
+                    <img src={generateAvatarURL} />
                 </div>
-            </div>
-            <div className='chat-header mb-2 font-bold'>
-                {msg.user}
-                <time className='opacity-70 ml-3 text-xs'>{msg.time}</time>
             </div>
             <p className='chat-bubble chat-bubble-info whitespace-pre-wrap'>
                 {msg.messageBody}
             </p>
+            <div className='chat-footer row-start-2 mb-2 font-bold'>
+                {msg.user}
+                <time className='opacity-70 ml-3 text-xs'>{msg.time}</time>
+            </div>
         </div>
     );
 }
